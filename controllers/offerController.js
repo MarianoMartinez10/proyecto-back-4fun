4269c1c64f6cc869584a0a514f1bba2d92fa8b97
Mariano <nanomartinezbm@gmail.com>
2026-04-26 20:30:30 -0300

    fix: remove asyncHandler from offerController

--- controllers/offerController.js
+++ controllers/offerController.js
@@ -1,51 +1,66 @@
 const OfferService = require('../services/offerService');
-const asyncHandler = require('../middlewares/asyncHandler');
 const ErrorResponse = require('../utils/errorResponse');
 
-exports.getOffers = asyncHandler(async (req, res) => {
-    const { productId } = req.params;
-    if (!productId) throw new ErrorResponse('El ID del producto es obligatorio', 400);
+exports.getOffers = async (req, res, next) => {
+    try {
+        const { productId } = req.params;
+        if (!productId) throw new ErrorResponse('El ID del producto es obligatorio', 400);
 
-    const offers = await OfferService.getOffersByProduct(productId);
+        const offers = await OfferService.getOffersByProduct(productId);
 
-    res.status(200).json({
-        success: true,
-        count: offers.length,
-        data: offers
-    });
-});
+        res.status(200).json({
+            success: true,
+            count: offers.length,
+            data: offers
+        });
+    } catch (error) {
+        next(error);
+    }
+};
 
-exports.createOffer = asyncHandler(async (req, res) => {
-    // Si viene dentro de /products/:productId/offers
-    const productId = req.params.productId || req.body.productId;
-    
-    const offerData = {
-        ...req.body,
-        productId
-    };
+exports.createOffer = async (req, res, next) => {
+    try {
+        // Si viene dentro de /products/:productId/offers
+        const productId = req.params.productId || req.body.productId;
+        
+        const offerData = {
+            ...req.body,
+            productId
+        };
 
-    const offer = await OfferService.createOffer(req.user.id, offerData);
+        const offer = await OfferService.createOffer(req.user.id, offerData);
 
-    res.status(201).json({
-        success: true,
-        data: offer
-    });
-});
+        res.status(201).json({
+            success: true,
+            data: offer
+        });
+    } catch (error) {
+        next(error);
+    }
+};
 
-exports.updateOffer = asyncHandler(async (req, res) => {
-    const offer = await OfferService.updateOffer(req.params.id, req.user.id, req.user.role, req.body);
+exports.updateOffer = async (req, res, next) => {
+    try {
+        const offer = await OfferService.updateOffer(req.params.id, req.user.id, req.user.role, req.body);
 
-    res.status(200).json({
-        success: true,
-        data: offer
-    });
-});
+        res.status(200).json({
+            success: true,
+            data: offer
+        });
+    } catch (error) {
+        next(error);
+    }
+};
 
-exports.deleteOffer = asyncHandler(async (req, res) => {
-    await OfferService.deleteOffer(req.params.id, req.user.id, req.user.role);
+exports.deleteOffer = async (req, res, next) => {
+    try {
+        await OfferService.deleteOffer(req.params.id, req.user.id, req.user.role);
 
-    res.status(200).json({
-        success: true,
-        data: {}
-    });
-});
+        res.status(200).json({
+            success: true,
+            data: {}
+        });
+    } catch (error) {
+        next(error);
+    }
+};
